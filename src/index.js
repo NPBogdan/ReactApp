71acebed11410d454397b87fd6a2248d329b9d85
Bogdan Norocel <46631986+NPBogdan@users.noreply.github.com>
2021-07-11 01:10:22 +0300

    Basic changes

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Bogdan</h1>
+    <h4>Razvan</h4>
+  </div>,
+  document.getElementById("root")
+);
